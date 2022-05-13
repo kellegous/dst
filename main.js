c22832f6e9db6b8d602fd0ab7f64a184982658de
Kelly Norton <kellegous@gmail.com>
2022-05-12 21:08:35 -0400

    .

--- main.js
+++ main.js
@@ -1,31 +1,71 @@
-function toRFC339(d) {
-	function p(v) {
-		return ('' + v).padStart(2, '0');
+// Never change, Javascript
+class Time {
+	constructor(
+		year,
+		month,
+		day,
+		hours,
+		minutes,
+		seconds,
+		offset
+	) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		this.hours = hours;
+		this.minutes = minutes;
+		this.seconds = seconds;
+		this.offset = offset;
 	}
 
-	function tz(offset) {
-		if (offset === 0) {
-			return 'Z';
+	toRFC3339() {
+		function p(v) {
+			return ('' + v).padStart(2, '0');
 		}
-		const sign = (offset > 0) ? '+' : '-',
-			v = Math.abs(offset),
-			h = (v / 60) | 0,
-			m = v % 60;
-		return `${sign}${p(h)}:${p(m)}`;
+
+		function tz(offset) {
+			if (offset === 0) {
+				return 'Z';
+			}
+			const sign = (offset > 0) ? '-' : '+',
+				v = Math.abs(offset),
+				h = (v / 60) | 0,
+				m = v % 60;
+			return `${sign}${p(h)}:${p(m)}`;
+		}
+
+		const { year, month, day, hours, minutes, seconds, offset } = this;
+		return `${year}-${p(month)}-${p(day)}T${p(hours)}:${p(minutes)}:${p(seconds)}${tz(offset)}`;
 	}
 
-	const ys = d.getFullYear() + '',
-		ms = p(d.getMonth() + 1),
-		ds = p(d.getDate()),
-		hs = p(d.getHours()),
-		ns = p(d.getMinutes()),
-		ss = p(d.getSeconds());
-	return `${ys}-${ms}-${ds}T${hs}:${ns}:${ss}${tz(d.getTimezoneOffset())}`;
+	static local(t) {
+		return new this(
+			t.getFullYear(),
+			t.getMonth() + 1,
+			t.getDate(),
+			t.getHours(),
+			t.getMinutes(),
+			t.getSeconds(),
+			t.getTimezoneOffset(),
+		);
+	}
 
+	static utc(t) {
+		return new this(
+			t.getUTCFullYear(),
+			t.getUTCMonth() + 1,
+			t.getUTCDate(),
+			t.getUTCHours(),
+			t.getUTCMinutes(),
+			t.getUTCSeconds(),
+			0
+		);
+	}
 }
 
 process.env.TZ = 'America/New_York';
 const ta = new Date(2021, 2, 14, 2, 0, 0);
-console.log(`${toRFC339(ta)}\t${ta.toISOString()}`);
+console.log(`${Time.local(ta).toRFC3339()}\t${Time.utc(ta).toRFC3339()}`);
+
 const tb = new Date(2021, 10, 7, 1, 0, 0, 0);
-console.log(`${toRFC339(tb)}\t${tb.toISOString()}`);
+console.log(`${Time.local(tb).toRFC3339()}\t${Time.utc(tb).toRFC3339()}`);
--- main.php
+++ main.php
@@ -1,8 +1,22 @@
 <?php
 
 $tz = new DateTimeZone('America/New_York');
+$utc = new DateTimeZone('UTC');
 
-$t = (new DateTime('now', $tz))
+$t = (new DateTimeImmutable('now', $tz))
 	->setDate(2021, 3, 14)
 	->setTime(2, 0, 0, 0);
-printf("%s\n", $t->format(DateTime::RFC3339));
+printf(
+	"%s\t%s\n",
+	$t->format(DateTime::RFC3339),
+	$t->setTimezone($utc)->format(DateTime::RFC3339)
+);
+
+$t = (new DateTimeImmutable('now', $tz))
+	->setDate(2021, 11, 7)
+	->setTime(1, 0, 0, 0);
+printf(
+	"%s\t%s\n",
+	$t->format(DateTime::RFC3339),
+	$t->setTimezone($utc)->format(DateTime::RFC3339)
+);
